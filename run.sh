4ea0f1c0e7cee4ba181e8d94e4107f3a5bbba3ec
Villermen <villermen@gmail.com>
2017-11-14 14:23:06 +0100

    Present net interface on websocket server

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ if [ "$INITIALIZATION_ERROR" -ne 0 ]; then
     echo "Initialization failed. Console not started!"
     exit 1
 else
-    ./bin/geth --datadir "$DATADIR" --networkid "$NETWORKID" --bootnodes "enode://e76d45675a61439e9a1649f56631bc0f2689bb7e95aecbf40e700d53f311ef75ac559cbcdfb9f00cb5652395c2478ea01239d233fc38a63d152ef8ed546778d3@88.159.207.186:30303" --rpc --rpccorsdomain "http://localhost:8000" console
+    ./bin/geth --datadir "$DATADIR" --networkid "$NETWORKID" --bootnodes "enode://e76d45675a61439e9a1649f56631bc0f2689bb7e95aecbf40e700d53f311ef75ac559cbcdfb9f00cb5652395c2478ea01239d233fc38a63d152ef8ed546778d3@88.159.207.186:30303" --rpc --rpccorsdomain "http://localhost:8000" --ws --wsport 8546 --wsorigins "*" --wsapi "eth,web3,net" console
 fi
 
 exit 0
