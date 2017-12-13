e8406cd0314436e47a78e1f68a5a1ea23db0e64a
Villermen <villermen@gmail.com>
2017-12-13 18:19:43 +0100

    Add binary for ARMv7 and mine script

--- run.bat
+++ run.bat
@@ -1,13 +1,17 @@
 @ECHO OFF
-REM I don't care for proper logic in this script because batch sucks.
 
-SET DATADIR=%~dp0data/
+REM Network parameters
+SET DATADIR=%~dp0data
 SET NETWORKID=9869
 
-%~dp0bin/geth.exe --datadir "%DATADIR%" init "genesis.json"
-%~dp0bin/geth.exe --datadir "%DATADIR%" --networkid "%NETWORKID%" ^
+REM Initialize data with genesis block
+%~dp0/bin/geth-windows-amd64.exe --datadir "%DATADIR%" init "genesis.json"
+
+REM Run node
+%~dp0/bin/geth-windows-amd64.exe --datadir "%DATADIR%" --networkid "%NETWORKID%" ^
 --bootnodes "enode://e76d45675a61439e9a1649f56631bc0f2689bb7e95aecbf40e700d53f311ef75ac559cbcdfb9f00cb5652395c2478ea01239d233fc38a63d152ef8ed546778d3@88.159.207.186:30303" ^
 --rpc --rpccorsdomain "http://localhost:8000" ^
 --ws --wsport 8546 --wsorigins "*" --wsapi "eth,web3,net" ^
 --txpool.pricelimit 0 --gasprice 0 ^
+--minerthreads 1 --etherbase "0x66974E872deaf3B9eF4a2EAa3689c8Fd00bC70FE" ^
 console
--- run.sh
+++ run.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-# Move to script directory
+# Move to directory of script
 cd "$(dirname $0)"
 
 # Network parameters
-DATADIR="data/"
+DATADIR=data/
 NETWORKID=9869
 
+# Decide binary based on architecture
+if [ $(arch) != "armv71" ]
+then
+    BINARY=geth-linux-amd64
+else
+    BINARY=geth-linux-arm7
+fi
+
 # Initialize data with genesis block
-if [ ! -d "$DATADIR" ]; then
-    ./bin/geth --datadir "$DATADIR" init "genesis.json"
-    INITIALIZATION_ERROR=$?
-else
-    INITIALIZATION_ERROR=0
-fi
+bin/$BINARY --datadir "$DATADIR" init "genesis.json"
 
-if [ "$INITIALIZATION_ERROR" -ne 0 ]; then
-    echo "Initialization failed. Console not started!"
-    exit 1
-else
-    ./bin/geth --datadir "$DATADIR" --networkid "$NETWORKID" \
-    --bootnodes "enode://e76d45675a61439e9a1649f56631bc0f2689bb7e95aecbf40e700d53f311ef75ac559cbcdfb9f00cb5652395c2478ea01239d233fc38a63d152ef8ed546778d3@88.159.207.186:30303" \
-    --rpc --rpccorsdomain "http://localhost:8000" \
-    --ws --wsport 8546 --wsorigins "*" --wsapi "eth,web3,net" \
-    --txpool.pricelimit 0 --gasprice 0 \
-    console
-fi
-
-exit 0
+# Run node
+bin/$BINARY --datadir "$DATADIR" --networkid "$NETWORKID" \
+--bootnodes "enode://e76d45675a61439e9a1649f56631bc0f2689bb7e95aecbf40e700d53f311ef75ac559cbcdfb9f00cb5652395c2478ea01239d233fc38a63d152ef8ed546778d3@88.159.207.186:30303" \
+--rpc --rpccorsdomain "http://localhost:8000" \
+--ws --wsport 8546 --wsorigins "*" --wsapi "eth,web3,net" \
+--txpool.pricelimit 0 --gasprice 0 \
+--minerthreads 1 --etherbase "0x66974E872deaf3B9eF4a2EAa3689c8Fd00bC70FE" \
+console
