31381bca0bcd743be53b71f97dbdb19e5e441816
Villermen <villermen@gmail.com>
2017-12-19 22:18:08 +0100

    Remove txpool.pricelimit from geth arguments
    Because it is set to its minimum anyway. Closes #3.

--- run.sh
+++ run.sh
@@ -43,6 +43,5 @@ bin/$BINARY --datadir "$DATADIR" --networkid "$NETWORKID" \
 --bootnodes "enode://e76d45675a61439e9a1649f56631bc0f2689bb7e95aecbf40e700d53f311ef75ac559cbcdfb9f00cb5652395c2478ea01239d233fc38a63d152ef8ed546778d3@88.159.207.186:30303" \
 --rpc --rpccorsdomain "http://localhost:8000" \
 --ws --wsport 8546 --wsorigins "*" --wsapi "eth,web3,net" \
---txpool.pricelimit 0 --gasprice 0 \
---minerthreads 1 --etherbase "0x66974E872deaf3B9eF4a2EAa3689c8Fd00bC70FE" \
+--gasprice 0 --minerthreads 1 --etherbase "0x66974E872deaf3B9eF4a2EAa3689c8Fd00bC70FE" \
 $GETH_ARGS
